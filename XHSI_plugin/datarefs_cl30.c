9dadc3320ce44795e0e92fca37133b738b038469
marrog <marrog.123@gmail.com>
2015-01-25 00:18:45 +0000

    Datarefs for Challenger300_XP10_31 update.

--- XHSI_plugin/datarefs_cl30.c
+++ XHSI_plugin/datarefs_cl30.c
@@ -43,7 +43,7 @@ int cl30_ready = 0;
 
 void findCL30DataRefs(void) {
 
-	cl30_plugin_status = XPLMFindDataRef("sim/custom/xap/poweron_p");
+	cl30_plugin_status = XPLMFindDataRef("cl300/poweron_p");
 
 	if ( cl30_plugin_status == NULL ) {
 
@@ -57,21 +57,21 @@ void findCL30DataRefs(void) {
 
             XPLMDebugString("XHSI: registering DDenn Design Bombardier Challenger 300 DataRefs\n");
 
-            cl30_v1 = XPLMFindDataRef("sim/custom/xap/refspds_v1");
-            cl30_vr = XPLMFindDataRef("sim/custom/xap/refspds_vr");
-            cl30_v2 = XPLMFindDataRef("sim/custom/xap/refspds_v2");
-            cl30_vt = XPLMFindDataRef("sim/custom/xap/refspds_vt");
-            cl30_vga = XPLMFindDataRef("sim/custom/xap/refspds_vga");
-            cl30_vref = XPLMFindDataRef("sim/custom/xap/refspds_vref");
+            cl30_v1 = XPLMFindDataRef("cl300/refspds_v1");
+            cl30_vr = XPLMFindDataRef("cl300/refspds_vr");
+            cl30_v2 = XPLMFindDataRef("cl300/refspds_v2");
+            cl30_vt = XPLMFindDataRef("cl300/refspds_vt");
+            cl30_vga = XPLMFindDataRef("cl300/refspds_vga");
+            cl30_vref = XPLMFindDataRef("cl300/refspds_vref");
 
-            cl30_refspds = XPLMFindDataRef("sim/custom/xap/refspds");
+            cl30_refspds = XPLMFindDataRef("cl300/refspds");
 
-            cl30_mast_warn = XPLMFindDataRef("sim/custom/xap/mast_warn");
-            cl30_mast_caut = XPLMFindDataRef("sim/custom/xap/mast_caut");
+            cl30_mast_warn = XPLMFindDataRef("cl300/mast_warn");
+            cl30_mast_caut = XPLMFindDataRef("cl300/mast_caut");
 
-            cl30_carets = XPLMFindDataRef("sim/custom/xap/carets");
-            cl30_to_n1 = XPLMFindDataRef("sim/custom/xap/TO_n1");
-            cl30_clb_n1 = XPLMFindDataRef("sim/custom/xap/CLB_n1");
+            cl30_carets = XPLMFindDataRef("cl300/carets");
+            cl30_to_n1 = XPLMFindDataRef("cl300/TO_n1");
+            cl30_clb_n1 = XPLMFindDataRef("cl300/CLB_n1");
 
 //            cl30_ = XPLMFindDataRef("");
 //            cl30_ = XPLMFindDataRef("");
