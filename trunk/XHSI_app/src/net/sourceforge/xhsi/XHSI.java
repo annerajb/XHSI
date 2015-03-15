32e327ba0293f04abb347ca8bb0292e662110dad
marrog <marrog@eff65ccb-6a2b-0410-b562-95f7822fc999>
2015-03-15 00:14:10 +0000

    Added a TX and an ID light to the RTU Display.

--- trunk/XHSI_app/src/net/sourceforge/xhsi/XHSI.java
+++ trunk/XHSI_app/src/net/sourceforge/xhsi/XHSI.java
@@ -82,7 +82,7 @@ import net.sourceforge.xhsi.util.XHSILogFormatter;
 public class XHSI implements ActionListener {
 
 
-    private static final String RELEASE = "2.0 Beta 8 Alpha 17";
+    private static final String RELEASE = "2.0 Beta 8 Alpha 18";
 
 
     public enum Mode { REPLAY, LIVE, RECORD }
