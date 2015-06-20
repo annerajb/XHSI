cbd7a8ed0fa5db0db6e0aff90ed86211b2af55b0
marrog <marrog.123@gmail.com>
2015-06-20 22:26:07 +0000

    Increased frequency for xfmc packets

--- XHSI_plugin/globals.h
+++ XHSI_plugin/globals.h
@@ -2,7 +2,7 @@
 // Global constants ============================
 
 // version info
-#define PLUGIN_VERSION_TEXT "XHSI Plugin 2.0 Beta 8 Alpha 30"
+#define PLUGIN_VERSION_TEXT "XHSI Plugin 2.0 Beta 8 Alpha 31"
 #define PLUGIN_VERSION_NUMBER 20008
 
 
