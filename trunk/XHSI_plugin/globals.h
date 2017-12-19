9ad9c4a4aa69aaf8a5340737884152d34446f0d3
marrog <marrog@eff65ccb-6a2b-0410-b562-95f7822fc999>
2017-12-19 19:43:17 +0000

    Preparing the version number for release

--- trunk/XHSI_plugin/globals.h
+++ trunk/XHSI_plugin/globals.h
@@ -2,7 +2,7 @@
 // Global constants ============================
 
 // version info
-#define PLUGIN_VERSION_TEXT "XHSI Plugin 2.0 Beta 10 Alpha 7"
+#define PLUGIN_VERSION_TEXT "XHSI Plugin 2.0 Beta 10"
 #define PLUGIN_VERSION_NUMBER 20010
 
 
